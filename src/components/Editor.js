8ce048041f1d41f72be5615b7472568666b24c1f
Matt Heslington <matt.heslington@icloud.com>
2020-10-05 11:17:25 +0800

    📒 Add localStorage

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
@@ -9,6 +9,9 @@ import 'codemirror/mode/css/css'
 
 import { Controlled as CodeEditor } from 'react-codemirror2'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
+
 export default function Editor(props) {
     const {
         language,
@@ -17,14 +20,22 @@ export default function Editor(props) {
         onChange
     } = props
 
+    const [open, setOpen] = useState(true)
+
     function handleChange(editor, data, value){
         onChange(value)
     }
     return (
-        <div className="editor-container">
+        <div className={`editor-container ${open ? '' : 'collapsed'}`}>
             <div className="editor-title">
                 {displayName}
-                <button>O/C</button>
+                <button
+                    type="button"
+                    className="expand-collapse-btn"
+                    onClick={() => setOpen(prevOpen => !prevOpen)}
+                >
+                    <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt}/>
+                </button>
             </div>
             <CodeEditor
                 onBeforeChange={handleChange}
