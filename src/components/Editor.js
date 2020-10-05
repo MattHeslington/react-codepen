c455fe37dda20c8ec19bea32b5263d25be5d9ec9
Matt Heslington <matt.heslington@icloud.com>
2020-10-05 10:41:41 +0800

    ✏️ Style code editors, add srcDoc

---
+++ src/components/Editor.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+
+import { Controlled as CodeEditor } from 'react-codemirror2'
+
+export default function Editor(props) {
+    const {
+        language,
+        displayName,
+        value,
+        onChange
+    } = props
+
+    function handleChange(editor, data, value){
+        onChange(value)
+    }
+    return (
+        <div className="editor-container">
+            <div className="editor-title">
+                {displayName}
+                <button>O/C</button>
+            </div>
+            <CodeEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className="code-mirror-wrapper"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    theme: 'material',
+                    mode: language,
+                    lineNumbers: true
+                }}
+            />
+        </div>
+    )
+}
